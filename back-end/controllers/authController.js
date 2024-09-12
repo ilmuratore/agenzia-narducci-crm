efb432243aeb83ac18b3b5c0d1cc47963f58c614
Simone Iengo <iengo.simone@gmail.com>
2024-09-12 12:21:17 +0200

    v7

--- back-end/controllers/authController.js
+++ back-end/controllers/authController.js
@@ -25,18 +25,4 @@ exports.login = async (req, res) => {
     }
 };
 
-// verifica token jwt 
-exports.verifyToken = (req, res, next) => {
-    const token = req.headers['authorization'];
-    if (!token) {
-        return res.status(403).json({ message: 'Accesso negato' });
-    }
 
-    try {
-        const decoded = jwt.verify(token.split(' ')[1], process.env.JWT_SECRET);
-        req.user = decoded;
-        next();
-    } catch (error) {
-        res.status(401).json({ message: 'Token non valido' });
-    }
-};
